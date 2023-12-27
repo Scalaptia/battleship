487cd18d613eb2703124e13ff55bed587299df75
Scalaptia <fernando@haroware.com>
2023-12-27 12:20:57 -0800

    Implement boards

--- src/components/board.ts
+++ src/components/board.ts
@@ -1,1 +1,31 @@
+import "../styles/board.css";
 
+export const board = (width: number, height: number) => {
+    const boardGrid = [];
+
+    for (let i = 0; i < height; i++) {
+        const row: HTMLDivElement[] = [];
+
+        for (let j = 0; j < width; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.push(cell);
+        }
+
+        boardGrid.push(row);
+    }
+
+    const boardEl = document.createElement("div");
+    boardEl.classList.add("board");
+
+    boardGrid.forEach((row) => {
+        row.forEach((cell) => {
+            boardEl.appendChild(cell);
+        });
+    });
+
+    return {
+        boardGrid,
+        boardEl,
+    };
+};
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "./styles/main.css";
+import { board } from "./components/board";
+
+const boardsContainerEl = document.getElementById("boards-container")!;
+const boards = [board(10, 10), board(10, 10)];
+
+boards.forEach((board) => {
+    boardsContainerEl.appendChild(board.boardEl);
+});
