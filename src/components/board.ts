abaeb90d8f1dd6028ac5e10e68470e8435acbddc
Scalaptia <fernando@haroware.com>
2024-03-17 21:55:15 -0700

    Fix ship collission when placing new ships

--- src/components/board.ts
+++ src/components/board.ts
@@ -16,29 +16,41 @@ export const createBoard = (width: number, height: number): Gameboard => {
             this.ships.push(ship);
 
             if (vertical) {
+                // Check wall collision
                 if (y + ship.length > this.boardGrid.length) {
                     this.ships.pop();
                     return false;
                 }
+
+                // Check ship collision
                 for (let i = 0; i < ship.length; i++) {
                     if (this.boardGrid[y + i][x].ship) {
                         this.ships.pop();
                         return false;
                     }
+                }
 
+                // Add ship to board
+                for (let i = 0; i < ship.length; i++) {
                     this.boardGrid[y + i][x].ship = ship;
                 }
             } else {
+                // Check wall collision
                 if (x + ship.length > this.boardGrid[0].length) {
                     this.ships.pop();
                     return false;
                 }
+
+                // Check ship collision
                 for (let i = 0; i < ship.length; i++) {
                     if (this.boardGrid[y][x + i].ship) {
                         this.ships.pop();
                         return false;
                     }
+                }
 
+                // Add ship to board
+                for (let i = 0; i < ship.length; i++) {
                     this.boardGrid[y][x + i].ship = ship;
                 }
             }
